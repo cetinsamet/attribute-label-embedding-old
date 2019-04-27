57298cd4b910313cf9152dc2b318e6f793dfdbc9
Samet Çetin <cet.samet@gmail.com>
2019-04-27 23:49:23 +0300

    add configurations

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+#
+# config.py
+#
+# Written by cetinsamet -*- cetin.samet@metu.edu.tr
+# April, 2019
+# --------------------------------------------------
+MAIN_DATAPATH   = 'data/APYP/'
+VAL_DATAPATH    = 'data/APYP/validation/'
+TEST_DATAPATH   = 'data/APYP/test/'
+OBJPATH         = 'obj.pickle'
